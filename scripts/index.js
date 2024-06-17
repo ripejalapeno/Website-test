a2f9da7bc732a9c6e925bccd3e7f19ccedb419e7
Ripe Jalapeno <cscotsman18@gmail.com>
2024-06-17 16:37:35 -0500

    Added text editor

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
 let vocabulary = [];
 
+// Load vocabulary from local storage if it exists
 let savedVocabulary;
-
 savedVocabulary = JSON.parse(localStorage.getItem('vocabulary'));
 if (savedVocabulary) {
   vocabulary = savedVocabulary;
 }
 
+// Load in elements from base doc
 const submitTextButton = document.querySelector('.js-submit-text-button');
 const textInputForm = document.querySelector('.js-text-input');
 const vocabularyDisplay = document.querySelector('.js-vocabulary-display');
 
+// Initial vocabulary display
 displayVocabulary();
+
+// Functions //
 
 function submitText() {
   submittedText = document.querySelector('.js-text-input').value;
@@ -33,15 +37,43 @@ function displayVocabulary()  {
   vocabulary.forEach((word) => {
     html = `
       <p>
-        ${word}
-        <button class="js-delete-button">
-          Delete
-        </button>
+        <div class="js-word-block">
+          <button class="vocab-word js-vocab-word">${word}</button>
+          <button class="js-delete-button">
+            Delete
+          </button>
+        <div>
       </p>
     `;
     vocabularyHTML += html;
   })
   vocabularyDisplay.innerHTML = vocabularyHTML;
+
+  const vocabWords = document.querySelectorAll('.js-vocab-word');
+  const wordBlocks = document.querySelectorAll('.js-word-block');
+  vocabWords.forEach((word, index) => {
+    word.addEventListener('click', () => {
+      wordBlocks[index].innerHTML = `
+        <input type="text" class="js-word-editor" value="${vocabulary[index]}">
+        <button class="js-update-button">
+          Update
+        </button>
+      `;
+
+      const wordEditor = document.querySelector('.js-word-editor');
+      const updateButton = document.querySelector('.js-update-button');
+
+      wordEditor.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+          updateWord(index, wordEditor.value);
+        }
+      });
+
+      updateButton.addEventListener('click', () => {
+        updateWord(index, wordEditor.value);
+      });
+    });
+  });
 
   const deleteButtons = document.querySelectorAll('.js-delete-button');
   deleteButtons.forEach((button, index) => {
@@ -49,11 +81,21 @@ function displayVocabulary()  {
       vocabulary.splice(index, 1);
 
       localStorage.setItem('vocabulary', JSON.stringify(vocabulary));
-      
+
       displayVocabulary();
     })
   })
 }
+
+function updateWord(index, value) {
+  vocabulary[index] = value;
+
+  localStorage.setItem('vocabulary', JSON.stringify(vocabulary));
+
+  displayVocabulary();
+}
+
+// General event listeners
 
 submitTextButton.addEventListener('click', submitText);
 
