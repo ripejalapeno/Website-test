764df67c60ef1c28143b7445889b19283e652506
Ripe Jalapeno <cscotsman18@gmail.com>
2024-06-17 15:22:56 -0500

    Added text input and vocabulary display

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,46 @@
-const message = 'Welcome to the home page';
+let vocabulary = [];
 
-const welcomeMessage = document.querySelector('.js-welcome-message');
+let savedVocabulary;
 
-welcomeMessage.innerText = message;
+savedVocabulary = JSON.parse(localStorage.getItem('vocabulary'));
+if (savedVocabulary) {
+  vocabulary = savedVocabulary;
+}
+
+const submitTextButton = document.querySelector('.js-submit-text-button');
+const textInputForm = document.querySelector('.js-text-input');
+const vocabularyDisplay = document.querySelector('.js-vocabulary-display');
+
+displayVocabulary();
+
+function submitText() {
+  submittedText = document.querySelector('.js-text-input').value;
+  submittedWords = submittedText.split(" ");
+
+  submittedWords.forEach((word) => {
+    vocabulary.push(word);
+  })
+
+  document.querySelector('.js-text-input').value = '';
+
+  localStorage.setItem('vocabulary', JSON.stringify(vocabulary));
+
+  displayVocabulary();
+}
+
+function displayVocabulary()  {
+  vocabularyHTML = '';
+  vocabulary.forEach((word) => {
+    html = `<p>${word}</p>`;
+    vocabularyHTML += html;
+  })
+  vocabularyDisplay.innerHTML = vocabularyHTML;
+}
+
+submitTextButton.addEventListener('click', submitText);
+
+textInputForm.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    submitText()
+  }
+});
